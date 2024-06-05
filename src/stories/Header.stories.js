8767fa3e21a44d73dbbb6e55270dffa6767a6a41
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-06-05 10:51:57 +0300

    cleanup

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -13,18 +13,18 @@ export default {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     // layout: 'centered'
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  // // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' }
-  },
+  // argTypes: {
+  //   backgroundColor: { control: 'color' }
+  // },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() }
 }
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary = {
+export const HeaderComponent = {
   args: {
     label: 'Header'
   }
